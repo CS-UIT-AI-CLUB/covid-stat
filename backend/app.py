6638f1ffbd4fcb30a80bea5989100eb147bc2d87
moriaty <19520624@gm.uit.edu.vn>
2021-07-15 12:13:47 +0000

    Fixed several errors on API backend

--- backend/app.py
+++ backend/app.py
@@ -8,26 +8,38 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb://' + \
                             os.environ['MONGO_INITDB_ROOT_USERNAME'] + ':' + \
                             os.environ['MONGO_INITDB_ROOT_PASSWORD'] + '@' + \
-                            'mongo:27017'
+                            'mongo:27017/covidstat?authSource=admin'
 mongo = PyMongo(app)
-db = mongo.covidstat
+db = mongo.db
 
-FETCH_INTERVAL = os.environ['FETCH_INTERVAL']
+FETCH_INTERVAL = int(os.environ['FETCH_INTERVAL'])
 
 def query_timestamp():
     timestamp = request.args.get('timestamp', default = int(time.time()), type = int)
-    t1 = timestamp - FETCH_INTERVAL//2
-    t2 = timestamp + FETCH_INTERVAL//2
-    return db.data.find({'timestamp', {'$gt': t1, '$lt': t2}})
+    t1 = timestamp - FETCH_INTERVAL
+    t2 = timestamp + FETCH_INTERVAL
+    return db.data.find_one({'timestamp': {'$gt': t1, '$lt': t2}})
 
 @app.route("/provinces", methods=["GET"])
 def get_provinces():
-    return query_timestamp()['provinces']
+    res = query_timestamp()
+    if res:
+        return res['provinces']
+    else:
+        return {}, 404
 
 @app.route("/vietnam", methods=["GET"])
 def get_vietnam():
-    return query_timestamp()['vietnam']
+    res = query_timestamp()
+    if res:
+        return res['vietnam']
+    else:
+        return {}, 404
 
 @app.route("/global", methods=["GET"])
 def get_global():
-    return query_timestamp()['global']
+    res = query_timestamp()
+    if res:
+        return res['global']
+    else:
+        return {}, 404
