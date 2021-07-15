8c7045ff3a93404cfcdaa26520b163efa0315c2d
moriaty <19520624@gm.uit.edu.vn>
2021-07-15 09:32:56 +0000

    Added MongoDB for worker

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 import schedule
 import time
+import os
+from pymongo import MongoClient
+
 from utils import fetch_data
+
+mongo_uri = 'mongodb://' + \
+                os.environ['MONGO_INITDB_ROOT_USERNAME'] + ':' + \
+                os.environ['MONGO_INITDB_ROOT_PASSWORD'] + '@' + \
+                'mongo:27017'
+
+client = MongoClient(mongo_uri)
+db = client.covidstat
 
 def job():
     data = fetch_data()
     t = int(time.time())
+    data['timestamp'] = t
     print('Done fetching data. Timestamp = {}'.format(t))
-    
+    db.data.insert_one(data)
+    print('Done inserting into database.')
     
 if __name__ == '__main__':
     print('Start worker')
